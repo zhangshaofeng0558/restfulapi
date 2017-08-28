5d4760a8c110dd9d68c5fde18953fdb317031dff
zhangshaofeng <zhangshaofeng0558@gmall.com>
2017-08-28 17:34:56 +0800

    get pagecount

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -26,6 +26,14 @@ class ArticleController extends ActiveController
         return ArrayHelper::merge(parent::behaviors(), [
             'corsFilter' => [
                 'class' => Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Allow-Credentials' => null,
+                    'Access-Control-Max-Age' => 86400,
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Page-Count'],
+                ],
             ],
             'authenticator' => [
                 'class' => QueryParamAuth::className(),
